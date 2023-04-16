7c3b8a656f9a3e4715afa414e0aecddc8a2ec8b5
Maciej Sadowski <263781@stuent.pwr,edu.pl>
2023-04-16 14:03:51 +0200

    implementacja metod do rozpoznawania pozycji pól oraz dodanie do funkcji play mozliwosci odkrywania pol

--- MSSFMLController.cpp
+++ MSSFMLController.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "MSSFMLController.h"
-
+#include <iostream>
 MSSFMLController::MSSFMLController(MinesweeperBoard & board, MSSFMLView &Sfmlboard) : B(board), G(Sfmlboard)
 {
 
@@ -11,7 +11,9 @@ MSSFMLController::MSSFMLController(MinesweeperBoard & board, MSSFMLView &Sfmlboa
 
 void MSSFMLController::play() {
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Saper");
+    sf::RenderWindow window(sf::VideoMode(get_window_width(), get_window_height()), "Saper");
+    window.setVerticalSyncEnabled(false);
+    window.setFramerateLimit(30);
 
     while (window.isOpen()) {
         sf::Event event{};
@@ -23,11 +25,41 @@ void MSSFMLController::play() {
         }
 
 
-            }
+       if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+       {
+           B.RevealdField(get_row_number(window), get_col_number(window));
+       }
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Right))
+        {
+            B.ToggleFlag(get_row_number(window), get_col_number(window));
         }
 
 
+        G.View(window);
+       window.display();
 
     }
+}
 
-    }
+int MSSFMLController::get_window_width() const {
+    return 150+G.Field_width(B.getwidth());
+}
+
+int MSSFMLController::get_window_height() const {
+    return 30+G.Field_height(B.getheight());
+}
+
+int MSSFMLController::get_col_number(sf::RenderWindow & window) const {
+    int px= sf::Mouse::getPosition(window).x;
+    int row=(px-150)/30;
+    return row;
+}
+
+int MSSFMLController::get_row_number(sf::RenderWindow &window) const {
+    int py= sf::Mouse::getPosition(window).y;
+    int col=(py-30)/30;
+    return col;
+}
+
+
+

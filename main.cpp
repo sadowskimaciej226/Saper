4914f408fac26035ecc8a55188cd17673aceb258
Maciej Sadowski <263781@stuent.pwr,edu.pl>
2023-04-26 18:09:16 +0200

    dodanie maina z gra

--- main.cpp
+++ main.cpp
@@ -4,31 +4,18 @@
 #include "GameManager.h"
 #include "SFMLGameMenu.h"
 #include "SFML_Board.h"
+#include "MSSFMLController.h"
 
 using namespace std;
 
 
 int main() {
-    SFMLGameMenu menu;
 
-
-    //MinesweeperBoard board(menu);
-    MinesweeperBoard board(7,5,Easy);
-
-
-   cout<<board.RevealdField(0,0)<<endl;
-    cout<<board.RevealdField(1,2)<<endl;
-    cout<<board.RevealdField(2,5)<<endl;
-    cout<<board.ToggleFlag(1,1);
-
-   // board.RevealdField(2,1);
-   // board.RevealdField(3,1);
-   //cout<<board.FieldInfo(0,0)<<endl;
-
-   //  cout<<board.getGameState();
-    MSSFMLView Game(board,menu);
+    MinesweeperBoard board(5,5,Easy);
+    MSSFMLView v(board);
     board.DebugDisplay();
-    Game.View();
+    MSSFMLController ctrl(board,v);
+    ctrl.play();
 
 return 0;
 }
